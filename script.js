c8753276a4e3fc471c8e5acd4bb92fb173d9b323
LusioF <lusiof@gmail.com>
2019-11-23 08:37:30 -0500

    add event listener

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ const randomFunc = {
 	number: getRandomNumber,
 	symbol: getRandomSymbol
 }
+
+clipboard.addEventListener('click', () => {
+	const textarea = document.createElement('textarea');
+	const password = resultEl.innerText;
+	
+	if(!password) { return; }
+
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
+});
